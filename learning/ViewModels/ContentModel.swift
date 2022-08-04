478bddf5482b6c84a9458a3f634bd483f252424b
Sarah Casagrande <sarah.e.casagrande@gmail.com>
2022-08-04 11:58:29 -0400

    Module 5 Lesson 3

--- learning/ViewModels/ContentModel.swift
+++ learning/ViewModels/ContentModel.swift
@@ -9,4 +9,44 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+        
+        let jsonData = try Data(contentsOf: jsonURL!)
+        let jsonDecoder = JSONDecoder()
+            
+        
+        let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+        self.modules = modules
+        
+        } catch {
+            print("Couldn't parse local data.")
+        }
+        // Parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            // Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        }
+        catch {
+            // Log error
+            print("Couldn't parse style data")
+        }
+        
+    }
 }
