6e1ac5d3b7101e82e2dc55032146bbb8da1ed839
Sarah Casagrande <sarah.e.casagrande@gmail.com>
2022-08-04 10:07:09 -0400

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Sarah Casagrande on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
