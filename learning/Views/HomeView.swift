ccba269a09b4ba7e467f4cfd9c555fdd85056225
Sarah Casagrande <sarah.e.casagrande@gmail.com>
2022-08-04 13:16:20 -0400

    Module 5 Lesson 4

--- learning/Views/HomeView.swift
+++ learning/Views/HomeView.swift
@@ -12,12 +12,39 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What Do You Want to Learn Today?")
+                    .padding(.leading, 20)
+                ScrollView {
+                    
+                    LazyVStack {
+                        
+                        
+                        ForEach(model.modules) { module in
+                            
+                            VStack(spacing: 20) {
+                            
+                            HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                            
+                            HomeViewRow(image: module.test.image, title: "Learn \(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                                
+                            }
+                            
+                        }
+                        
+                    }.padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
